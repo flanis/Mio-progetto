677a64bffde9dce686730fe3c0f1ffc4aecb5e22
flaminionisco <flaminionisco@airdiflnionisco.lan>
2015-03-24 17:38:39 +0100

    primo commento

---
+++ templates/protostar/html/mod_joompro_articles_author/default.php
@@ -0,0 +1,7 @@
+<?php JLoader::register('JHtmlString', JPATH_LIBRARIES . '/joomla/html/html/string.php'); ?>
+<ul class="latestnews<?php echo $moduleclass_sfx; ?>">
+<?php foreach ($list as $item) : ?>
+<li><a href="<?php echo $item->link; ?>"><?php echo $item->title; ?></a></li>
+<p><?php echo JHtmlString::truncate(strip_tags($item->introtext), 50); ?></p>
+<?php endforeach; ?>
+</ul>
